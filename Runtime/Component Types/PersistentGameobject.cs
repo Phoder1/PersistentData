bae102873da78797e992dc8ecc5ceac5fd7b5f60
Alon Talmi <35352105+Phoder1@users.noreply.github.com>
2021-11-26 16:05:52 +0200

    PersistentGameobject

---
+++ Runtime/Component Types/PersistentGameobject.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace Phoder1.PersistentData
+{
+    [AddComponentMenu("Phoder1/Persistent Data/Gameobject data")]
+    public class PersistentGameobject : BaseKeySOData<GameObject> { }
+}
